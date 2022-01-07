766ed2a8b0cab8712dd8bae2f0829a70f25b04c0
Rodrigo Xavier <rodrigo.henrique@take.net>
2022-01-07 13:59:42 -0300

    UseState and UseEffect working

--- src/hooks/UseStateAndUseEffectExample.tsx
+++ src/hooks/UseStateAndUseEffectExample.tsx
@@ -4,8 +4,12 @@ export default function UseStateAndUseEffectExample() {
     const [counter, setCounter] = useState(0);
 
     useEffect(() => {
-        
-    }, [])
+        document.title = counter.toString();
+
+        return(() => {
+            document.title = "React App";
+        })
+    }, [counter])
 
     return (
         <>
