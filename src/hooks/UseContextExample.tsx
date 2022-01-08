4b39d1df4b6327e3ec0d89c7e2d385eea460c4f5
Rodrigo Xavier <rodrigo.henrique@take.net>
2022-01-08 14:45:02 -0300

    UseContext working and explaining a custom hook used

--- src/hooks/UseContextExample.tsx
+++ src/hooks/UseContextExample.tsx
@@ -1,17 +1,8 @@
-import {useContext, useEffect, useState} from 'react';
 import { useThemeContext } from '../App';
 import PageHeader from '../Components/Header';
 
 const UseContextExample = () => {
     const themeContext = useThemeContext();
-    const [counter, setCounter] = useState(0);
-
-    useEffect(() => {
-        document.title = counter.toString();
-        return(() => {
-            document.title = "React Hooks - Exemplos";
-        })
-    }, [counter])
 
     return (
         <>
@@ -21,6 +12,13 @@ const UseContextExample = () => {
                     style={{color: themeContext.textColor}}
                 >
                     Esse texto está baseado na cor de tema definido no contexto.
+                </h3>
+            </div>
+            <div className="main-centered-div" style={{background: themeContext.backgroundColor}}>
+                <h3
+                    style={{color: themeContext.textColor}}
+                >
+                    Para receber o thema vindo do App.ts usamos o 'useThemeContext' que é um hook personalizado exportado com o contexto que exitia no momento da criação da aplicação.
                 </h3>
             </div>
         </>
