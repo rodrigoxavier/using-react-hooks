8c4ecf3354101e65788fb028773bb1bc51dc8010
Rodrigo Xavier <rodrigo.henrique@take.net>
2022-01-09 21:33:28 -0300

    Creating examples with useCallback

--- src/hooks/UseCallBackExample.tsx
+++ src/hooks/UseCallBackExample.tsx
@@ -1,22 +1,43 @@
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import PageHeader from '../Components/Header';
 
 const UseCallbackExample = () => {
-    const [counter, setCounter] = useState(0);
+    const [firstEntry, setFirstEntry] = useState(0);
+    const [secondEntry, setSecondEntry] = useState(0);
+    const [entriesSum, setEntriesSum] = useState(0);
 
-    useEffect(() => {
-        document.title = counter.toString();
-        return(() => {
-            document.title = "React Hooks - Exemplos";
-        })
-    }, [counter])
+    const sumEntries = () => {
+        setEntriesSum(firstEntry + secondEntry);
+    }
+
+    const sumEntriesCallbackNotDeps = useCallback(() => {
+        setEntriesSum(firstEntry + secondEntry);
+    }, []);
+
+    const sumEntriesCallbackWithDeps = useCallback(() => {
+        setEntriesSum(firstEntry + secondEntry);
+    }, [firstEntry, secondEntry]);
 
     return (
         <>
             <PageHeader backToHome={true} title="useCallback"/>
             <div className="main-centered-div">
-                <h3 className="app-main-text">{counter}</h3> &nbsp;
-                <button className="back-button-style" onClick={() => setCounter(counter + 1)}>Incrementar</button>
+                <input type="number" name="firstEntry" value={firstEntry} onChange={e => setFirstEntry(parseInt(e.target.value))}/> &nbsp;
+                <span className="app-main-text">+</span> &nbsp;
+                <input type="number" name="firstEntry" value={secondEntry} onChange={e => setSecondEntry(parseInt(e.target.value))}/>
+            </div>
+            <div className="main-centered-div">
+                <span className="app-main-text">Soma: </span> &nbsp;
+                <span className="app-main-text">{entriesSum}</span> &nbsp;
+            </div>
+            <div className="main-centered-div">
+                <button className="back-button-style" onClick={() => sumEntries()}>Somar normalmente</button>
+            </div>
+            <div className="main-centered-div">
+                <button className="back-button-style" onClick={() => sumEntriesCallbackNotDeps()}>Somar useCB sem deps</button>
+            </div>
+            <div className="main-centered-div">
+                <button className="back-button-style" onClick={() => sumEntriesCallbackWithDeps()}>Somar useCB com deps</button>
             </div>
         </>
     );
